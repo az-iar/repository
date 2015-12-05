f3263373ecb608121671dc31c143dd14e6f60b7d
Azri Ismail <azri@fashionvalet.com>
2015-12-05 20:00:17 +0800

    stub repository

---
+++ stub/Eloquent/User.php
@@ -0,0 +1,8 @@
+<?php namespace Inneuron\Repository\Stubs\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+
+}
---
+++ stub/Eloquent/UserRepository.php
@@ -0,0 +1,8 @@
+<?php namespace Inneuron\Repository\Stubs\Eloquent;
+
+use Inneuron\Repository\Eloquent\BaseRepository;
+
+class UserRepository extends BaseRepository
+{
+    protected $model = User::class;
+}
